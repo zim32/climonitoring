c58bbf965e891a321341ea11315aefb8d64a4d44
zim32 <zim32@ukr.net>
2019-06-05 19:40:16 +0300

    Static or shared build

---
+++ build_shared.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# EXECUTE AS ROOT
+
+go install -buildmode=shared -linkshared std
+
+# metrics
+go build -linkshared -o ./bin/cm_m_filestat ./metrics/filestat
+go build -linkshared -o ./bin/cm_m_cpu ./metrics/cpu
+go build -linkshared -o ./bin/cm_m_ram ./metrics/ram
+go build -linkshared -o ./bin/cm_m_tcp ./metrics/tcp
+
+# filters
+go build -linkshared -o ./bin/cm_f_false ./filters/false
+go build -linkshared -o ./bin/cm_f_true ./filters/true
+go build -linkshared -o ./bin/cm_f_regex ./filters/regex
+
+# processors
+go build -linkshared -o ./bin/cm_p_bulk ./processors/bulk
+go build -linkshared -o ./bin/cm_p_message ./processors/message
+go build -linkshared -o ./bin/cm_p_eot2nl ./processors/eot2nl
+go build -linkshared -o ./bin/cm_p_nl2eot ./processors/nl2eot
+go build -linkshared -o ./bin/cm_p_debounce ./processors/debounce
+go build -linkshared -o ./bin/cm_p_truncate ./processors/truncate
+go build -linkshared -o ./bin/cm_p_watchdog ./processors/watchdog
+
+# outputs
+go build -linkshared -o ./bin/cm_o_telegram ./outputs/telegram
+go build -linkshared -o ./bin/cm_o_opsgenie ./outputs/opsgenie
+go build -linkshared -o ./bin/cm_o_smtp ./outputs/smtp
